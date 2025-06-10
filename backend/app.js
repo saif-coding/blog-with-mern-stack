5ce440592f6a3b4d8a88fe6d16dc5ec09fd9a067
saif ullah <saifullaha730@gmail.com>
2025-06-10 11:42:25 +0500

    backend url added

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://blog-with-mern-stack.onrender.com",
     credentials: true,
   })
 );
