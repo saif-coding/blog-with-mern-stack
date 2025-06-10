b48db475f62f3f2019299b787e2d40aa0f3141d5
saif ullah <saifullaha730@gmail.com>
2025-06-10 12:16:12 +0500

    another issue resovle

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Hero from "../components/Hero";
-import FeaturedPosts from "../components/FeaturedPosts.";
+import FeaturedPosts from "../components/FeaturedPosts";
 import Categories from "../components/Categories";
 import AboutBlog from "../components/AboutBlog";
 import Newsletter from "../components/Newsletter";
